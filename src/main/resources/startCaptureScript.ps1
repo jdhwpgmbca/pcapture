7e09d797a151bbcecc5f376ec7694539c0081d72
James Holmes <jdh@rtds.com>
2021-08-12 17:23:38 -0500

    Some commentary updates regarding Ethernet interface selection.

--- src/main/resources/startCaptureScript.ps1
+++ src/main/resources/startCaptureScript.ps1
@@ -4,5 +4,7 @@ $env:Path+= ";C:\Program Files\Wireshark"
 # Start the dumpcap program in another window, writing the PID to the standard output.
 
 # You'll probably want to add a capture filter such as -f ""ether proto 0x99B8" for Goose. Other useful EtherTypes: 0x99B9 for GSE, and 0x88BA for SV.
+# You'll also likely need to rename vEthernetBridge to whatever network interface you're using on Windows. It's likely that you'll have several if you're
+# using Docker or HyperV.
 
-(Start-Process -FilePath "dumpcap.exe" -ArgumentList "-i vEthernet -w $param1" -PassThru).Id
+(Start-Process -FilePath "dumpcap.exe" -ArgumentList "-i vEthernetBridge -w $param1" -PassThru).Id
