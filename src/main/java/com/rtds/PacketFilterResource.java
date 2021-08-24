36b39462d51dd507f345b03094bfb06ad6cc51d3
James Holmes <jdh@rtds.com>
2021-08-23 20:57:40 -0500

    Added auto-refresh checkbox and refresh button.
    This works around problems with browser refresh issues.

--- src/main/java/com/rtds/PacketFilterResource.java
+++ src/main/java/com/rtds/PacketFilterResource.java
@@ -10,13 +10,7 @@ import com.rtds.svc.CaptureTypeService;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
 
 /**
  *
