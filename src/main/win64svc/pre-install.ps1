7a4c25b32421f7b73f392b1c4296b2186549c5e6
James Holmes <jdh@rtds.com>
2021-08-26 23:02:10 -0500

    Add some of the admin scripts to win64svc install folder.
    This is mostly just for convenience. It makes it very easy to setup the service on a development workstation because everything you need to install and configure the service is all in one directory. Just run the pre-install.ps1 script, then the install-and-start.ps1 script to get the service running, then the add-all-filters.ps1 script to populate the capture filters. For production you can copy the directory to your production server, change the .env file to point to it, and have the proper certificates, and update the dumpcap-ws.xml file to use a valid PKCS12 server certificate.

--- src/main/win64svc/pre-install.ps1
+++ src/main/win64svc/pre-install.ps1
@@ -1,2 +1,6 @@
 Copy-Item -Path ${project.basedir}/src/main/resources/startCaptureScript.ps1 -Destination .
 Copy-Item -Path ${project.build.directory}/${project.artifactId}-${project.version}-runner.jar -Destination .
+Copy-Item -Path ${project.basedir}/.env -Destination .
+Copy-Item -Path ${project.basedir}/admin_scripts/add-all-filters.ps1 -Destination .
+Copy-Item -Path ${project.basedir}/admin_scripts/add-filter.ps1 -Destination .
+Copy-Item -Path ${project.basedir}/admin_scripts/delete-filter.ps1 -Destination .
