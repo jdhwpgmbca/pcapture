f257eca00a5936749d229d165093bf72f510843f
James Holmes <jdh@rtds.com>
2021-08-31 23:27:34 -0500

    Removed references to our docker image repository.

--- dockerBuild.ps1
+++ dockerBuild.ps1
@@ -1,4 +1,4 @@
 
 .\mvnw clean compile package -Pcontainer
 $ENV:DOCKER_BUILDKIT=1
-docker build -f target/docker/Dockerfile.alpine -t quarkus/pcapture-jvm .
+docker build -f target/docker/Dockerfile.alpine -t pcapture:latest .
--- dockerRun.ps1
+++ dockerRun.ps1
@@ -1,2 +1,2 @@
 
-docker run --rm -p8080:8080 --privileged git.rtdstech.com:4567/jdh/pcapture:latest
+docker run --rm -p8080:8080 --privileged --mount source=pcapture,target=/data pcapture:latest
